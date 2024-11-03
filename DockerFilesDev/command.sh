7fd2a44e724a644740d41d343597fad93dc37ae2
Swarup Bhise <swarupbhise420@gmail.com>
2024-11-04 01:15:38 +0530

    Added Jenkins Pipeline File

--- DockerFilesDev/command.sh
+++ DockerFilesDev/command.sh
@@ -1,2 +1,6 @@
 # to run this Testing Docker File in Development Mode
-docker compose --env-file ../.env up
+docker compose --env-file ../.env -f <FILE_NAME> up
+
+
+# to run the postrgress
+docker run -d --name my_postgres -e POSTGRES_USER=myuser -e POSTGRES_PASSWORD=mypassword -e POSTGRES_DB=mydatabase -p 5432:5432 postgres:latest
